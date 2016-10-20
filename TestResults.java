24bf6c2db027414007f82e42b7711222be9eadf1
Laflammexx <ga300@rutgers.edu>
2016-10-20 19:34:49 -0400

    Static counter

--- TestResults.java
+++ TestResults.java
@@ -1,4 +1,4 @@
-//Giorgi Amirajibi
+//Gio Amirajibi
 
 import java.util.Scanner;
 
@@ -11,15 +11,15 @@ public class TestResults
         int counter, experiment;
         for (experiment = 1; experiment <= 4; experiment++)
         {
-            System.out.println("Experiment " + experiment);
-            for (counter = 1; counter <= 6; counter++) {
-                System.out.println("Please enter test result #" + counter);
-                testResults = keyboard.nextDouble();
-                sum += testResults;
-            }
-            average = sum / (counter-1);
-            sum = 0;
-            System.out.println("Average result for experiment #" + experiment + " = " + average);
+                for (counter = 1; counter <= 6; counter++)
+                {
+                    System.out.println("Please enter test result #" + counter);
+                    testResults = keyboard.nextDouble();
+                    sum += testResults;
+                }
+                average = sum / (counter - 1);
+                sum = 0;
+                System.out.println("Average result for experiment #" + experiment + " = " + average);
         }
     }
 }
